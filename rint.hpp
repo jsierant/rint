e35b6ee55aea6e00caf899a49e41d03eec5b57e7
Jaroslaw Sierant <jaroslaw.sierant@gmail.com>
2018-04-20 22:51:29 +0200

    verify_range extracted

--- rint.hpp
+++ rint.hpp
@@ -28,6 +28,14 @@ struct value_type<min, max, void, head, candidates...> {
 template<auto min, auto max>
 struct value_type<min, max, void> { };
 
+template<typename R, typename Val>
+static void verify_range(Val val, char const* msg) {
+  if(std::numeric_limits<R>::min() > val
+    || std::numeric_limits<R>::max() < val) {
+    throw std::out_of_range(msg);
+  }
+}
+
 }
 
 template<auto min_v, auto max_v>
@@ -47,7 +55,7 @@ public:
   template<typename Val>
   explicit ranged_int(Val val)
     : value(static_cast<value_type>(val)) {
-    verify(val);
+      detail::verify_range<value_type>(val, "rint: failed to initialize");
   }
 
   ranged_int() : value(0) { }
@@ -88,21 +96,12 @@ public:
   }
 
 private:
-  template<typename Val>
-  static void verify(Val val) {
-    if(std::numeric_limits<value_type>::min() > val
-      || std::numeric_limits<value_type>::max() < val) {
-      throw std::out_of_range("rint: failed to initilize");
-    }
-  }
   value_type value;
 };
 
 template<typename t, auto min, auto max>
 t to_integral(ranged_int<min, max> val) {
-  if(!detail::in_range<min, max, t>()) {
-    throw std::out_of_range("rint: unable to cast");
-  }
+  detail::verify_range<t>(*val, "rint: unable to cast");
   return static_cast<t>(*val);
 }
 
